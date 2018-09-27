52d66542a0974218f30dcbc2699820279750dde0
abh1906 <jain.abhinav2406@gmail.com>
2018-09-27 15:24:03 +0530

    final

--- 7-sep-18-task/ProductFilter.java
+++ 7-sep-18-task/ProductFilter.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class ProductFilter{
 
-    public void searchBypname(HashSet<Product> list,String pname){
+    public void searchBypname(ArrayList<Product> list,String pname){
         for(Product pro:list){
             if(pro.getpName().equalsIgnoreCase(pname)){
                 System.out.println("ProductCode:"+pro.getpCode()+" "+"Productname:"+pro.getpName()+" "+"Brand:"+pro.getBrand()+" "+"Quantity:"+pro.getQuantity()+" "+"Price:"+pro.getPrice());
@@ -11,19 +11,57 @@ public class ProductFilter{
 
 
     }
-    public void searchBybrand(HashSet<Product> list,String brand){
+    public void searchBybrand(ArrayList<Product> list,String brand){
         for(Product pro:list){
             if(pro.getBrand().equalsIgnoreCase(brand)){
                 System.out.println("ProductCode:"+pro.getpCode()+" "+"Productname:"+pro.getpName()+" "+"Brand:"+pro.getBrand()+" "+"Quantity:"+pro.getQuantity()+" "+"Price:"+pro.getPrice());
-   
             }
-    public void searchByprice(HashSet<Product> list,int price){
+            }
+        }
+    public void searchByprice(ArrayList<Product> list,int price){
         for(Product pro:list){
             if(pro.getPrice()==price){
                 System.out.println("ProductCode:"+pro.getpCode()+" "+"Productname:"+pro.getpName()+" "+"Brand:"+pro.getBrand()+" "+"Quantity:"+pro.getQuantity()+" "+"Price:"+pro.getPrice());
    
             }
+   
+    
+        }
+        }
+    public void filterByprice(ArrayList<Product> list,String brand){
+        Collections.sort(list, new Comparator<Product>() 
+        {
+            public int compare(Product a,Product b){
+                if((a.getPrice()<=b.getPrice()))
+                return -1;
+                else
+                 return 1;
+            }
+        });
+        for(Product pro:list){
+            if(pro.getBrand().equalsIgnoreCase(brand))
+            System.out.println("ProductCode:"+pro.getpCode()+" "+"Productname:"+pro.getpName()+" "+"Brand:"+pro.getBrand()+" "+"Quantity:"+pro.getQuantity()+" "+"Price:"+pro.getPrice());
+   
+        }
+    
     }
+    public void filterByquantity(ArrayList<Product> list,String brand){
+        Collections.sort(list, new Comparator<Product>() 
+        {
+            public int compare(Product a,Product b){
+                if((a.getQuantity()<=b.getQuantity()))
+                return -1;
+                else
+                 return 1;
+            }
+        });
+        for(Product pro:list){
+            if(pro.getBrand().equalsIgnoreCase(brand))
+            System.out.println("ProductCode:"+pro.getpCode()+" "+"Productname:"+pro.getpName()+" "+"Brand:"+pro.getBrand()+" "+"Quantity:"+pro.getQuantity()+" "+"Price:"+pro.getPrice());
+   
         }
     }
+    
+
+    
 }
--- 7-sep-18-task/ProductFilterClient.java
+++ 7-sep-18-task/ProductFilterClient.java
@@ -1,5 +1,6 @@
 
 import java.io.*;
+import java.util.ArrayList;
 public class ProductFilterClient{
     public static void main(String args[]){
         ArrayList <Product> list=new ArrayList<>();
@@ -13,6 +14,23 @@ public class ProductFilterClient{
         list.add(new Product(108,"Maestro","Hero",60000,20));
         list.add(new Product(109,"Baleno","Nexa",500000,20));
         list.add(new Product(110,"Fzs","Suzuki",850000,20));
-       
+        list.add(new Product(111,"city","Honda",550000,10));
+       ProductFilter pf=new ProductFilter();
+       System.out.println("search by brand");
+       pf.searchBybrand(list,"Honda");
+
+       System.out.println("search by product name");
+       pf.searchBypname(list,"maestro");
+
+       System.out.println("search by price");
+       pf.searchByprice(list,55000);
+
+       System.out.println("Filter by price");
+       pf.filterByprice(list,"Honda");
+
+       System.out.println("Filter by quantity");
+       pf.filterByquantity(list,"Honda");
+
+
     }
 }
